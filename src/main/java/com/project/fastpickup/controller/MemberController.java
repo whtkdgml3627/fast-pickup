7a4589de5108f12aabe564a73b753bfb43eab9ed
kwonjonny <128343377+kwonjonny@users.noreply.github.com>
2023-07-25 11:12:30 +0900

    commit message ju yoong lee beoung shin

--- src/main/java/com/project/fastpickup/controller/MemberController.java
+++ src/main/java/com/project/fastpickup/controller/MemberController.java
@@ -1,4 +1,20 @@
 package com.project.fastpickup.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Controller
+@RequestMapping("/member/")
 public class MemberController {
+
+    @GetMapping("list")
+    public String getListMember() {
+        log.info("GET | Member List");
+        return "/member/list";
+    }
+    
 }
