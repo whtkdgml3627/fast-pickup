ce1f7fb0cdafdfd6fd121354059cff86abe384ce
kwonjonny <128343377+kwonjonny@users.noreply.github.com>
2023-07-25 14:33:56 +0900

    lee jy beong shin

--- src/main/java/com/project/fastpickup/dto/MemberDTO.java
+++ src/main/java/com/project/fastpickup/dto/MemberDTO.java
@@ -1,4 +1,23 @@
 package com.project.fastpickup.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberDTO {
+    private String email;
+    private String memberPw;
+    private String memberName;
+    private String memberPhone;
+    private boolean store;
+    private String storeName;
 }
